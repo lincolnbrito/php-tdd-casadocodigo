c03a5858143feea1335bad62ee16b7ac23de7e33
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-06-08 18:27:56 -0300

    implement calculator

--- tests/CDC/Exemplos/CalculadoraTest.php
+++ tests/CDC/Exemplos/CalculadoraTest.php
@@ -11,4 +11,25 @@ class CalculadoraTest extends TestCase
         $this->assertEquals(4, (new Calculadora())->soma(2,2));
         $this->assertEquals(2, (new Calculadora())->soma(1,1));
     }
+
+    public function testDeveSomarPositivoComNegativo()
+    {
+        $this->assertEquals(4, (new Calculadora())->soma(6,-2));
+    }
+
+    public function testDeveSomarNegativoComPositivo()
+    {
+        $this->assertEquals(-4, (new Calculadora())->soma(-6,2));
+    }
+
+    public function testDeveSomarDoisNumerosNegativos()
+    {
+        $this->assertEquals(-4, (new Calculadora())->soma(-2,-2));
+    }
+
+    public function testDeveSomarComZero()
+    {
+        $this->assertEquals(4, (new Calculadora())->soma(4,0));
+        $this->assertEquals(4, (new Calculadora())->soma(0,4));
+    }
 }
