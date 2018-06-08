03334b10cde06b4ac66abbc85d04575fc12e5e26
Lincoln Brito <lincoln.sbrito@gmail.com>
2018-06-08 17:36:43 -0300

    finish cart tests

--- src/CDC/Loja/Carrinho/CarrinhoDeCompras.php
+++ src/CDC/Loja/Carrinho/CarrinhoDeCompras.php
@@ -31,11 +31,11 @@ class CarrinhoDeCompras
             return 0;
         }
 
-        $maiorValor = $this->getProdutos()[0]->getValor();
+        $maiorValor = $this->getProdutos()[0]->getValorUnitario();
 
         foreach($this->getProdutos() as $produto) {
-            if($maiorValor < $produto->getValor()) {
-                $maiorValor = $produto->getValor();
+            if($maiorValor < $produto->getValorUnitario()) {
+                $maiorValor = $produto->getValorUnitario();
             }
         }
 
--- tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
+++ tests/CDC/Loja/Carrinho/CarrinhoDeComprasTest.php
@@ -15,4 +15,27 @@ class CarrinhoDeComprasTest extends TestCase
 
         $this->assertEquals(0, $valor, null, 0.00001);
     }
+
+    public function testDeveRetornarValorDosItensSeCarrinhoCom1Elemento()
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+
+        $valor = $carrinho->maiorValor();
+
+        $this->assertEquals(900.00, $valor, null, 0.00001);
+    }
+
+    public function testDeveRetornarMaiorValorSeCarrinhoComMuitosElementos()
+    {
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        $carrinho->adiciona(new Produto("Fogão", 1500.00, 1));
+        $carrinho->adiciona(new Produto("Máquina de lavar", 750.00, 1));
+
+        $valor = $carrinho->maiorValor();
+
+        $this->assertEquals(1500.00, $valor, null, 0.00001);
+    }
 }
